f336c88e64f4794866319355e7ba51f2c598e38b
chaima <cheima.bouchaham@univ-constantine2.dz>
2024-09-27 18:30:57 +0100

    style changes

--- src/Pages/NotFound.js
+++ src/Pages/NotFound.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const NotFound =() => {
     return (
         <div className='notFound'>
-            <h3>The Page You Are Looking For Doesn't Exist </h3>
+            <h3> Click home to check CHEIMA's portfolio  </h3>
             <NavLink exact to='/'>
                 <i className='fas fa-home'></i>
                 <span> Home</span>
